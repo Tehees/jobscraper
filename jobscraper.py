357b4d7341e573a86c02739a19c23604d8c5f716
Stephen Lau <stephenkala@hotmail.com>
2019-02-04 17:06:23 -0500

    Add files via upload
    added more filters and displays relevant job matches

--- jobscraper.py
+++ jobscraper.py
@@ -5,12 +5,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-URL = "https://www.indeed.com/jobs?q=data+scientist+%2420%2C000&l=New+York&start=10"
-
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.text, "html.parser")
-
 def extract_job_title_from_result(soup):
   jobs = []
   for div in soup.find_all(name="div", attrs={"class":"row"}):
@@ -38,10 +32,57 @@ def extract_location_from_result(soup):
     locations.append(span.text)
   return(locations)
 
-output3 = extract_location_from_result(soup)
-output2 = extract_company_from_result(soup)
-output = extract_job_title_from_result(soup)
+def extract_salary_from_result(soup): 
+  salaries = []
+  for div in soup.find_all(name="div", attrs={"class":"row"}):
+    try:
+      salaries.append(div.find("nobr").text)
+    except:
+      try:
+        div_two = div.find(name="div", attrs={"class":"sjcl"})
+        div_three = div_two.find("div")
+        salaries.append(div_three.text.strip())
+      except:
+        salaries.append("Nothing_found")
+  return(salaries)
 
-combined_dict = dict(zip(output,output2))
-print(combined_dict)
-print(output3)
+def extract_summary_from_result(soup): 
+  summaries = []
+  spans = soup.findAll("span", attrs={"class": "summary"})
+  for span in spans:
+    summaries.append(span.text.strip())
+  return(summaries)
+
+
+def find_all_results():
+
+  max_results_per_city = 100
+  #city_set = ["New+York","Chicago","San+Francisco", "Austin", "Seattle", "Los+Angeles", "Philadelphia", "Atlanta", "Dallas", "Pittsburgh", "Portland", "Phoenix", "Denver", "Houston", "Miami", "Washington+DC", "Boulder"]
+  city_set = ["New York","Toronto", "San Francisco"]
+  types_of_jobs = ["Software Engineer", "Bank", "Consulting"]
+  columns = ["city", "job_title", "company_name", "location", "summary", "salary"]
+
+  for city in city_set:
+    for start in range(0, max_results_per_city):
+      page = requests.get("http://www.indeed.com/jobs?q=data+scientist+%2420%2C000&l=" + str(city) + "&start=" + str(start))
+      #time.sleep(0.05)  #ensuring at least 1 second between page grabs
+      soup = BeautifulSoup(page.text, "html.parser")
+      
+      location_results = extract_location_from_result(soup)
+      company_results = extract_company_from_result(soup)
+      job_title_results = extract_job_title_from_result(soup)
+
+      job_info = list(zip(location_results, company_results))
+    
+    
+  job_results = dict(zip(job_title_results,job_info))
+  print(job_results)
+
+find_all_results()
+#output3 = extract_location_from_result(soup)
+#output2 = extract_company_from_result(soup)
+#output = extract_job_title_from_result(soup)
+
+##combined_dict = dict(zip(output,output2))
+#print(combined_dict)
+#print(output3)
